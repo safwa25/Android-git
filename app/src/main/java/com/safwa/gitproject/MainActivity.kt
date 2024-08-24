64b34f8fc2ed3bf1e10cf4769e407fb11c182876
Mansour <Abdomans7712>
2024-08-24 16:00:00 +0300

    first commit in about branch

--- app/src/main/java/com/safwa/gitproject/MainActivity.kt
+++ app/src/main/java/com/safwa/gitproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.safwa.gitproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d("MyLog", "Hello from about")
     }
 }
